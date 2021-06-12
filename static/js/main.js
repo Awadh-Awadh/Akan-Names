233906caf339296aaebe5f96d117db5b5a7c9d8f
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-06-12 22:29:38 +0300

    store the values input by users

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+
+var button = document.querySelector('#btn');
+var year = document.getElementById("year").value;
+var month = document.getElementById("month").value;
+var day = document.getElementById("day").value;
+var maleGender = document.getElementById("male").value;
+var femaleGender = document.getElementById("female").value;
+
+
+
+
+
+
+
+
+
+// button.addEventListener('click', ()=> {
+//  let results = document.querySelector('#results');
+//  results.style.opacity= 1;
+//  console.log("awadh");
+// })
